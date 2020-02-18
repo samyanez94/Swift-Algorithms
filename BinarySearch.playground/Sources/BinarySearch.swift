1661c0c57b9a0a5846853c30f4b310bf2cce208a
Samuel Yanez <samuelyanez94@gmail.com>
2020-02-18 14:53:57 -0800

    Improve readability of binary search

--- BinarySearch.playground/Sources/BinarySearch.swift
+++ BinarySearch.playground/Sources/BinarySearch.swift
@@ -13,19 +13,18 @@ public extension RandomAccessCollection where Element: Comparable {
     /// - Returns: The index of the value in the collection if found.
     func binarySearch(for value: Element, in range: Range<Index>? = nil) -> Index? {
         let range = range ?? startIndex..<endIndex
-        if range.lowerBound >= range.upperBound {
-            return nil
+        
+        guard range.lowerBound < range.upperBound else { return nil }
+        
+        let middleIndex = index(range.lowerBound, offsetBy: distance(from: range.lowerBound, to: range.upperBound) / 2)
+        
+        if self[middleIndex] > value {
+            return binarySearch(for: value, in: range.lowerBound..<middleIndex)
+        } else if self[middleIndex] < value {
+            return binarySearch(for: value, in: index(after:
+            middleIndex)..<range.upperBound)
         } else {
-            let size = distance(from: range.lowerBound, to: range.upperBound)
-            let middleIndex = index(range.lowerBound, offsetBy: size / 2)
-            if self[middleIndex] > value {
-                return binarySearch(for: value, in: range.lowerBound..<middleIndex)
-            } else if self[middleIndex] < value {
-                return binarySearch(for: value, in: index(after:
-                middleIndex)..<range.upperBound)
-            } else {
-                return middleIndex
-            }
+            return middleIndex
         }
     }
 }
